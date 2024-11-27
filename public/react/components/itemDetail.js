d2ca0f19edf697e972864c7ef89d9d0e43be347b
James Nguyen <james.nguyen@james.nguyen-MacBook-Pro>
2024-11-27 09:04:42 -0800

    Add buttons for edit and delete

--- public/react/components/itemDetail.js
+++ public/react/components/itemDetail.js
@@ -10,13 +10,17 @@ export const ItemDetail = ({item,onBack}) => {
     <>
       <div className='body'>
         <div className='item'>
+        <Button onClick={onBack} variant='secondary' className='back-button'> Back </Button>
           <div className='image-wrap'> <img src={item.image} alt={item.name} className='image'/></div>
           <div className='content'>
             <div className='description'> Description: {item.description} </div>
             <div className='price'> Price: ${item.price} </div>
             <div className='category'> Category: {item.category} </div>
           </div>
-          <Button onClick={onBack} variant='primary' className='back-button'> Back </Button>
+          <div className='actions'>
+            <Button variant='primary' className='edit-button'> Edit </Button>
+            <Button variant='danger' className='delete-button'> Delete </Button>
+          </div>
         </div>
       </div>
       <br/>
